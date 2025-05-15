8871a5e268fae4deb21a45cc5fd46dbfe3ecd8b4
Caio Caminha <caminhacaiopro@gmail.com>
2025-05-15 06:24:40 -0300

    adding comments

--- src/main/java/com/caiocaminha/javamultithreading/guardedblock/Drop.java
+++ src/main/java/com/caiocaminha/javamultithreading/guardedblock/Drop.java
@@ -12,11 +12,20 @@ public class Drop {
     public synchronized String getMessage() {
         while(empty) {
             try {
+                /**
+                 * The Object.wait method will suspend the thread until a notification is sent
+                 * But the notification that causes the Interruption on wait not necessarily is the one we are waiting for
+                 * Because of that we have a try catch, if we receive the InterruptionException but the condition still the same
+                 * we block the thread again.
+                 */
                 wait();
             }catch (InterruptedException e) {}
         }
         empty = true;
 
+        /**
+         * This will notify the ProducerThread that the condition has changed and the messages are empty again
+         */
         notifyAll();
         return message;
     }
@@ -24,11 +33,21 @@ public class Drop {
     public synchronized void putMessage(String message) {
         while(!empty) {
             try{
+                /**
+                 * The Object.wait method will suspend the thread until a notification is sent
+                 * But the notification that causes the Interruption on wait not necessarily is the one we are waiting for
+                 * Because of that we have a try catch, if we receive the InterruptionException but the condition still the same
+                 * we block the thread again.
+                 */
                 wait();
             } catch (InterruptedException e) {}
         }
         empty = false;
         this.message = message;
+
+        /**
+         * This will notify the ConsumerThread that the condition has changed and the messages are not empty anymore
+         */
         notifyAll();
     }
 }
