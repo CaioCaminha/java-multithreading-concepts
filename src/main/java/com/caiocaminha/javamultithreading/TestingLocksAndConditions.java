328808cd58435e7d2b2e43a4adf610a18170a8d4
Caio Caminha <caminhacaiopro@gmail.com>
2025-05-21 06:31:52 -0300

    adding comments regarding locks and conditions

--- src/main/java/com/caiocaminha/javamultithreading/TestingLocksAndConditions.java
+++ src/main/java/com/caiocaminha/javamultithreading/TestingLocksAndConditions.java
@@ -5,6 +5,12 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class TestingLocksAndConditions {
+    /**
+     * Will use the same wait-notify process that using wait and notify objects from Object Monitor
+     * notFull and notEmpty are the conditions
+     * while the items is not empty we wait until something is added and a signal is emmited
+     * while the items is not full we wait until all elements are added and a signal is emmited
+     */
     class BoundedBuffer {
         final Lock lock = new ReentrantLock();
         final Condition notFull = lock.newCondition();
