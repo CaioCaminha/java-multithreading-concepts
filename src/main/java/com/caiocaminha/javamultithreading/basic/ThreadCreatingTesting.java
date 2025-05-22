b87616cff9a3d719cd02e996b2c9026487ac70bd
Caio Caminha <caminhacaiopro@gmail.com>
2025-05-22 08:56:34 -0300

    testing setUncaughtExceptionHandler

---
+++ src/main/java/com/caiocaminha/javamultithreading/basic/ThreadCreatingTesting.java
@@ -0,0 +1,23 @@
+package com.caiocaminha.javamultithreading.basic;
+
+public class ThreadCreatingTesting {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            throw new RuntimeException("Intentional Exception thrown");
+        });
+
+        thread.setName("CreatedThread");
+
+        /**
+         * I'm allowed to pass a lambda to setUncaughtExceptionHandler becase it's expecting a UncaughtExceptionHandler
+         * This interface is a Functional interface, meaning that only has one abstract method
+         * void uncaughtException(Thread t, Throwable e); This way JVM already knows that this lambda is an implementation
+         * of this FunctionalInterface. Without needing to speficy a Function type for example.
+         */
+        thread.setUncaughtExceptionHandler((Thread t, Throwable e) -> {
+            System.out.format("Critical error detected in thread: %s exception is: %s", t.getName(), e.getMessage());
+        });
+
+        thread.start();
+    }
+}
